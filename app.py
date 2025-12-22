dfec4824412df680b4c47f70c04ddad3af812c1d
Yoshihiro-Kai-Dev <yoshihirokai13@gmail.com>
2025-12-22 19:28:20 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1670,7 +1670,7 @@ def predict_race(df, model_pack, encoders, _engine):
             else: o = float(o_str)
         except: o = 0.0
         
-        if row['is_pace_advantage'] == 1 and p >= 0.05 and 10.0 <= o <= 100.0:
+        if row['is_pace_advantage'] == 1 and p >= 0.05 and 20.0 <= o <= 50.0:
             return "🚀 展開ブースト", "-"
         if p >= 0.08 and 50.0 <= o <= 100.0:
             return "-", "💣 穴馬ブースト"
